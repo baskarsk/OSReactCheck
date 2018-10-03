957167391a5f640e699936a137a89fbf459c4355
baskarsk <Baskar.Saravanan@cognizant.com>
2018-10-03 18:18:31 +0530

    Uncommented for ReactApp Check
    Uncommented for ReactApp Check

--- app.js
+++ app.js
@@ -3,20 +3,20 @@ var morgan = require(`morgan`);
 
 
 // // Comment out during production
-// var webpack = require('webpack');
-// var WebpackDevServer = require('webpack-dev-server');
-// var config = require('./webpack.dev');
-// new WebpackDevServer(webpack(config), {
-//   publicPath: config.output.publicPath,
-//   hot: true,
-//   historyApiFallback: true
-// }).listen(3001, 'localhost', function (err, result) {
-//   if (err) {
-//     return console.log(err);
-//   }
-//
-//   console.log('Listening at http://localhost:3001/');
-// });
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.dev');
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true
+}).listen(3001, 'localhost', function (err, result) {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log('Listening at http://localhost:3001/');
+});
 
 
 // Init express
